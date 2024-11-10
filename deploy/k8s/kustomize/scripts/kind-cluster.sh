f512559ae61640b9deb1f4b670a9b191b768db82
Bailey Hayes <behayes2@gmail.com>
2024-11-10 13:06:28 -0700

    feat(kustomize): use wasmcloud-platform chart
    - Change to use wasmcloud-platform chart
    - Tested locally with latest kind, helm, and nginx
    - Bumped ingress-nginx according to kind install guide
    
    Signed-off-by: Bailey Hayes <behayes2@gmail.com>

--- deploy/k8s/kustomize/scripts/kind-cluster.sh
+++ deploy/k8s/kustomize/scripts/kind-cluster.sh
@@ -1,53 +1,42 @@
 #!/bin/bash
 set -e -x -o pipefail
 
-kind create cluster --config=deploy/kind/cluster.yaml
+kind create cluster --config=deploy/kind/cluster.yaml --wait 30s
 
 #############
 # Ingress
 #############
 kubectl apply -k deploy/kind
 
-#############
-# Nats
-#############
-# prefix with helm chart seems to break things down.
-# workaround by helm templating instead of kustomize:
-kubectl apply -f deploy/base/charts-rendered/nats.yaml
-
-# wait or nats to be ready
-kubectl rollout status deploy,sts -l app.kubernetes.io/instance=nats
-
-# test nats:
-kubectl exec -it deployment/nats-box -- nats pub test hi
-
-##############################
-# wasmCloud operator and wadm
-##############################
-# we apply Ingress at this step, validate it's ready
 kubectl wait --namespace ingress-nginx \
   --for=condition=ready pod \
   --selector=app.kubernetes.io/component=controller \
   --timeout=90s
 
-kustomize build --enable-helm ./deploy/dev | kubectl apply -f -
+######################
+# wasmCloud platform
+######################
+kubectl apply -k ./deploy/dev
 
-# validate wasmcloud-operator
-kubectl rollout status deploy -l app.kubernetes.io/name=wasmcloud-operator
+# wait for nats
+kubectl rollout status deploy,sts -l app.kubernetes.io/name=nats
+
+# wait for wadm
+kubectl wait --for=condition=available --timeout=600s deploy -l app.kubernetes.io/name=wadm
+
+# wait for wasmcloud-operator
+kubectl wait --for=condition=available --timeout=600s deploy -l app.kubernetes.io/name=wasmcloud-operator
 
 # validate operator added OAM CRD
 kubectl wait --for condition=available apiservices.apiregistration.k8s.io v1beta1.core.oam.dev
 
 # Configure wasmCloud host
-kustomize build ./deploy/dev/hosts | kubectl apply -f -
+kubectl apply -k ./deploy/dev/hosts
 kubectl wait --for condition=established --timeout=60s crd/wasmcloudhostconfigs.k8s.wasmcloud.dev
 
-echo "Validate wadm deployment"
-kubectl rollout status deploy -l app.kubernetes.io/instance=wadm
-
 ##############################
-# Deploy Go and Rust apps
-##############################
+# Deploy apps
+############################## 
 kustomize build ./deploy/dev/apps | kubectl apply -f -
 
 # wait for http service
--- deploy/k8s/kustomize/scripts/update-helm.sh
+++ deploy/k8s/kustomize/scripts/update-helm.sh
@@ -2,34 +2,12 @@
 
 # Following practices from https://github.com/kubernetes-sigs/kustomize/blob/master/examples/chart.md
 
-# Pass in -r to remove the charts directory
 rm -rf deploy/base/charts
 
-WADM_CHART_VERSION=0.2.5
-WASMCLOUD_OPERATOR_CHART_VERSION=0.1.5
-NATS_CHART_VERSION=1.2.2
+WASMCLOUD_PLATFORM_CHART_VERSION=0.1.0
 
-helm pull oci://ghcr.io/wasmcloud/charts/wadm \
-    --version $WADM_CHART_VERSION \
+helm pull oci://ghcr.io/wasmcloud/charts/wasmcloud-platform \
+    --version $WASMCLOUD_PLATFORM_CHART_VERSION \
     --untar \
     --untardir deploy/base/charts
-helm template wadm --values ./deploy/base/wadm-values.yaml --version $WADM_CHART_VERSION ./deploy/base/charts/wadm > deploy/base/charts-rendered/wadm.yaml
-
-helm pull oci://ghcr.io/wasmcloud/charts/wasmcloud-operator \
-    --version $WASMCLOUD_OPERATOR_CHART_VERSION \
-    --untar \
-    --untardir deploy/base/charts
-helm template wasmcloud-operator --version $WASMCLOUD_OPERATOR_CHART_VERSION ./deploy/base/charts/wasmcloud-operator > deploy/base/charts-rendered/wasmcloud-operator.yaml
-
-# ugh this should work but gzip header error on the tgz, helm isn't handling redirects
-# helm pull https://nats-io.github.io/k8s/helm/charts/ --version ^1.2 \
-#     --untar \
-#     --untardir deploy/base/charts/nats
-# workaround:
-helm repo add nats https://nats-io.github.io/k8s/helm/charts/
-helm repo update
-helm pull nats/nats --version $NATS_CHART_VERSION
-tar -xvf nats-${NATS_CHART_VERSION}.tgz -C deploy/base/charts
-helm template nats --values ./deploy/base/nats-values.yaml --version $NATS_CHART_VERSION ./deploy/base/charts/nats > deploy/base/charts-rendered/nats.yaml
-# cleanup
-rm nats-1.2.2.tgz
+helm template wasmcloud-platform --values ./deploy/base/values.yaml --version $WASMCLOUD_PLATFORM_CHART_VERSION ./deploy/base/charts/wasmcloud-platform > deploy/base/charts-rendered/wasmcloud-platform.yaml
